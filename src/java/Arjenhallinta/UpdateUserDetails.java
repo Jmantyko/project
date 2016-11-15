e0df990e7fecc9fb231d404055d83e43b241323b
Jaakko Heikkilä <jaakkoheikkila@outlook.com>
2016-11-15 22:34:15 +0200

    UpdateUserDetails servlet siivoilua

--- src/java/Arjenhallinta/UpdateUserDetails.java
+++ src/java/Arjenhallinta/UpdateUserDetails.java
@@ -6,7 +6,6 @@
 package Arjenhallinta;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,34 +16,6 @@ import javax.servlet.http.HttpServletResponse;
  * @author Jaakko
  */
 public class UpdateUserDetails extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-    }
-
-    
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-    }
 
     /**
      * Handles the HTTP <code>POST</code> method.
@@ -71,15 +42,4 @@ public class UpdateUserDetails extends HttpServlet {
         response.sendRedirect(request.getContextPath() + "/profile.jsp");
         
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }
-
 }
